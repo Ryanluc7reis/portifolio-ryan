d5331fb9ad3b0595d5e4086ad6e1eb2d8452b4aa
Ryan Lucas <ryanlucasfrfrfrfr@hotmail.com>
2023-06-01 22:58:32 -0300

    Removido imagem de loading

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -2,35 +2,11 @@ import styled from 'styled-components'
 const StyledFlex = styled.div`
   width: 100%;
   height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: #fbfbfb;
-`
-const StyledImage = styled.div`
-  background-image: url('${(props) => props.image}');
-  background-repeat: no-repeat;
-  background-size: cover;
-  padding: 160px;
-  margin-right: 80px;
-  @media (max-width: 768px) {
-    padding: 100px;
-    align-items: center;
-    display: flex;
-    justify-content: center;
-  }
+  background: #ffffff;
 `
 
-function Loading({ children, image }) {
-  return (
-    <StyledFlex>
-      {children}
-      <StyledImage image={image} />
-    </StyledFlex>
-  )
+function Loading({ children }) {
+  return <StyledFlex>{children}</StyledFlex>
 }
 
-Loading.defaultProps = {
-  image: '/loading.png'
-}
 export default Loading
